23eca57729cb7aea013b546d32b9a8606cd84a59
bfbfghb <63604829+bfbfghb@users.noreply.github.com>
2021-04-23 22:11:36 -0300

    NLW-5 GameQuiz

---
+++ nlw_1/lib/challenge/challenge_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+
+class ChallengeController{
+  final currentPageNotifier = ValueNotifier<int>(0);
+  int get currentPage => currentPageNotifier.value;
+  set currentPage(int value) => currentPageNotifier.value = value;
+
+  int qtdAnwserRight = 0;
+}
---
+++ nlw_1/lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/awnser_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
+
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final ValueChanged<bool> onSelected;
+
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onSelected,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+  AwnserModel awnser(int index) => widget.question.awnsers[index];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        SizedBox(height: 64,),
+        Text(widget.question.title, style: AppTextStyles.heading),
+        SizedBox(height: 24),
+        for(var i = 0; i < widget.question.awnsers.length; i++)
+        AnswerWidget(
+          anwser: awnser(i),
+          disabled: indexSelected != -1,
+          isSelected: indexSelected == i,
+          onTap: (value) { 
+            indexSelected = i;
+            setState(() {});
+            Future.delayed(Duration(milliseconds: 100)).then((_) => widget.onSelected(value));
+           },
+          ),
+        ]      
+      ),
+    );
+  }
+}
